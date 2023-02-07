f4f7ded9c232e3486def2f6f534553b138ccf289
lukas <legendluke25@gmail.com>
2023-02-07 17:23:18 -0500

    Wrote get data test

--- tests.py
+++ tests.py
@@ -5,14 +5,16 @@ import database_setup
 def test_get_data():
     url = 'https://lukasdearruda.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries.json'
     responses = get_from_api(url)
+    values = responses.get("Entries")
+    assert len(values) == 3
 
-
-def test_database():
-    test_response = {
-        'Entries': [{'EntryId': '1', 'Field1': 'Mr.', 'Field2': 'Joe', 'Field3': 'Schmoe', 'Field4': 'Dude',
-                     'Field6': 'Company Inc.', 'Field7': 'therealjoeschmoe@company.net', 'Field8': '',
-                     'Field9': '1234567890', 'Field10': 'Guest Speaker', 'Field11': 'Spring 2023', 'Field14': 'Yes',
-                     'DateCreated': '2023-01-27 13:12:13', 'CreatedBy': 'public', 'DateUpdated': '', 'UpdatedBy': None
-                     }]}
-    connection, cursor = database_setup.create_database("testdb")
-    database_setup.write_response_to_database(test_response, cursor)
+#
+# def test_database():
+#     test_response = {
+#         'Entries': [{'EntryId': '1', 'Field1': 'Mr.', 'Field2': 'Joe', 'Field3': 'Schmoe', 'Field4': 'Dude',
+#                      'Field6': 'Company Inc.', 'Field7': 'therealjoeschmoe@company.net', 'Field8': '',
+#                      'Field9': '1234567890', 'Field10': 'Guest Speaker', 'Field11': 'Spring 2023', 'Field14': 'Yes',
+#                      'DateCreated': '2023-01-27 13:12:13', 'CreatedBy': 'public', 'DateUpdated': '', 'UpdatedBy': None
+#                      }]}
+#     connection, cursor = database_setup.create_database("testdb")
+#     database_setup.write_response_to_database(test_response, cursor)
