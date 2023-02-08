3ded101c5f28c6e761b3b364cecd3e7d2733fcaa
lukas <legendluke25@gmail.com>
2023-02-08 08:54:32 -0500

    Fully implemented test_database()

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ COMP 490 Project 1 Sprint 2
 Need to install requests, and create a file called secrets.py that 
 contains api_key
 
-This project grabs data from a specified API in json format and saves it to database. The database has one table with
-12 columns: entryNum, prefix, fName, lName, title, orgName, email, ,orgSite, phoneNum, opportunities, collabTime, permission
+This project grabs data from a specified API in json format and saves it to database. 
 
-Currently, partially missing database test. current test creates a test database and writes a test response to it,
-but does not check for if that data is the same as what is initially pushed 
+The database has one table with 12 columns: 
+entryNum, prefix, fName, lName, title, orgName, email, ,orgSite, phoneNum, opportunities, collabTime, permission
+
+Nothing is missing from this project
+
--- tests.py
+++ tests.py
@@ -13,8 +13,7 @@ def test_database():
     test_response = {
         'Entries': [{'EntryId': '1', 'Field1': 'Mr.', 'Field2': 'Joe', 'Field3': 'Schmoe', 'Field4': 'Dude',
                      'Field6': 'Company Inc.', 'Field7': 'therealjoeschmoe@company.net', 'Field8': '',
-                     'Field9': '1234567890', 'Field10': 'Guest Speaker', 'Field11': 'Spring 2023', 'Field14': 'Yes',
-                     'DateCreated': '2023-01-27 13:12:13', 'CreatedBy': 'public', 'DateUpdated': '', 'UpdatedBy': None
+                     'Field9': '1234567890', 'Field10': 'Guest Speaker', 'Field11': 'Spring 2023', 'Field14': 'Yes'
                      }]}
 
     connection, cursor = database_setup.open_db("testdb.db")
@@ -22,6 +21,19 @@ def test_database():
             email, orgSite, phoneNum, opportunities, collabTime, permission)""")
     database_setup.write_response_to_database(test_response, cursor)
     connection.commit()
-    cursor.execute("""SELECT * FROM responses WHERE entryNum = 1""")
+
+    # Fetch the response from the table
+    cursor.execute("""SELECT * FROM responses""")
+
+    # Take the entry retrieved from the db and make it into a list
+    entry_from_db = cursor.fetchall()
     database_setup.close_db(connection)
-1
+    entry_from_db = list(entry_from_db[0])
+
+    # Make a list of the values from the initial response
+    initial_entry = test_response.get("Entries")
+    entry_values = list(initial_entry[0].values())
+
+    # See if the two lists are equal
+    assert entry_from_db == entry_values
+
